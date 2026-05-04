bcd047b8cd60684c36fa8f240c4e53dbd9f34138
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-05-04 23:23:06 +0530

    Add: palindrome-partitioning - submission-4

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-4.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(string s, int idx, vector<vector<string>>& res, vector<string>& subset){
+        if(idx>=s.size()){
+            res.push_back(subset);
+            return;
+        }
+
+        for(int i=idx; i<s.size(); i++){
+            if(isPali(s, idx, i)){
+                subset.push_back(s.substr(idx, i-idx+1));
+                dfs(s, i+1, res, subset);
+                subset.pop_back();
+            }
+        }
+    }
+
+    bool isPali(string s, int l, int r){
+        while(l<r)
+            if(s[l++]!=s[r--])
+                return false;
+        return true;
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> subset;
+        dfs(s, 0, res, subset);
+        return res;
+    }
+};
