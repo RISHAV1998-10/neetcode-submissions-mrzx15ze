c5bdac5bfc8c759335960bb4007d1d79572d5d0b
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-05-12 01:55:29 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void dfs(vector<int>& nums, int target, int i, vector<vector<int>>& res, vector<int>& subset){
+        if(i==nums.size()-1 && target==0){
+            res.push_back(subset);
+            return;
+        }
+        if(target <0 || i>=nums.size())
+            return;
+
+        subset.push_back(nums[i]);
+        dfs(nums, target-nums[i], i, res, subset);
+        subset.pop_back();
+
+        dfs(nums, target, i+1, res, subset);
+            
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        vector<int> subset;
+        dfs(nums, target, 0, res, subset);
+        return res;
+    }
+};
