d292ec9284f2b36899ad425671bb7b06c93425f7
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-06-05 20:54:33 +0530

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isCycle(int idx, unordered_map<int, vector<int>>& preMap, vector<int> state){
+        if(state[idx]==1)
+            return true;
+
+        // if(state[idx]==2)
+        //     return false;
+        
+        state[idx] = 1;
+        
+        for(int i: preMap[idx]){
+            if(isCycle(i, preMap, state))
+                return true;
+        }
+
+        state[idx]=2;
+
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> preMap;
+
+        for(auto& p: prerequisites)
+            preMap[p[0]].push_back(p[1]);
+
+        vector<int> state(numCourses, 0);
+
+        for(int i=0; i<numCourses; i++){
+            if(state[i]==0 && isCycle(i, preMap, state))
+                return false;
+        }
+
+        return true;
+    }
+};
