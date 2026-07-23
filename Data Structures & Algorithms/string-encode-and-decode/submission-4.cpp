247034315ba9081a33e97f8ccedec5e2f2e5aae5
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-07-24 01:00:20 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string str = "";
+        for(auto s: strs){
+            string n = to_string(s.size());
+            str+=n+"#"+s;
+        }
+
+        return str;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        int i=0;
+        while(i<s.size()){
+            int n=0;
+            while(s[i]!='#' && isdigit(s[i])){
+                n=n*10+(s[i]-'0');
+                i++;
+            }
+            res.push_back(s.substr(i+1, n));
+            i=i+1+n;
+        }
+
+        return res;
+    }
+};
