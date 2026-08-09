c577b2427926dfced73efc1aef11510a70f3250a
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-08-10 00:13:16 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,33 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int, string>>> umap;    
+public:
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        umap[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = umap.find(key);
+        if(it == umap.end())
+            return "";
+
+        auto &v = it->second;
+        int l=0, r=v.size()-1;
+        string ans="";
+        
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(v[mid].first <= timestamp){
+                ans=v[mid].second;
+                l=mid+1;
+            }
+            else
+                r=mid-1;
+        }
+
+        return ans;
+    }
+};
