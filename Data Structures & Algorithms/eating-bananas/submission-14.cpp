dcc7a188053513b5d65907847d029cc1977ed0aa
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-08-09 16:36:25 +0530

    Add: eating-bananas - submission-14

---
+++ Data Structures & Algorithms/eating-bananas/submission-14.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int time(vector<int>& piles, int rate){
+        int t=0;
+        for(int p:piles){
+            t+=ceil(p/(double)rate);
+        }
+        return t;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int maxR = *max_element(piles.begin(), piles.end());
+        int l=1, r=maxR, res=maxR;
+        
+        while(l<=r){
+            int rate=(l+r)/2;
+            int t=time(piles, rate);
+            if(t<=h){
+                if(res>rate)
+                    res=rate;
+                r=rate-1;
+            }
+            else
+                l=rate+1;
+        }
+
+        return res;
+    }
+};
