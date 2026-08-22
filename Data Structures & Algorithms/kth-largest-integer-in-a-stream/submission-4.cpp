cbe92186da00d6753d98c1073215ceabd0ac402f
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-08-22 22:35:52 +0530

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.cpp
@@ -0,0 +1,23 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int cap;
+    KthLargest(int k, vector<int>& nums) {
+        cap = k;
+        for(int n: nums){
+            pq.push(n);
+            if(pq.size()>k)
+                pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>cap)
+            pq.pop();
+
+
+        return pq.top();
+
+    }
+};
