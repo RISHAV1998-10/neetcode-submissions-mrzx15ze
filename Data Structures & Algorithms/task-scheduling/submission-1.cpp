5000c95bf904471d59b0b80dade066122225694a
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-08-23 14:16:43 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int len = tasks.size();
+        int maxF = 0;
+        int maxCnt = 0;
+        unordered_map<int, int> freq;
+
+        for(int i=0; i<tasks.size(); i++)
+            freq[tasks[i]]++;
+        
+        for(auto [t, f] : freq)
+            if(f>maxF)
+                maxF=f;
+
+        for(auto [t, f] : freq)
+            if(maxF==f)
+                maxCnt++;
+
+        cout<<"maxF: "<<maxF<<" maxCnt: "<<maxCnt<<endl;
+
+        int numOfSeq = maxF-1;
+        int seqLen = n-maxCnt+1;
+        int nonMaxTask = len - maxCnt*maxF;
+        int idleTime = numOfSeq * seqLen - nonMaxTask;
+
+        return idleTime>0 ? len+idleTime : len;
+
+    }
+};
