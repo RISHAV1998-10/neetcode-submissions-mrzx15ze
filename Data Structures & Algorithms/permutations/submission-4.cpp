09b4dc0b3b5c0996661fbf68ebdb5de813b6606b
RISHAV1998-10 <rishavchakraborty1998@gmail.com>
2026-09-15 12:43:41 +0530

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void dfs(vector<int>& nums, int idx, vector<vector<int>>& res, vector<int>& perm, unordered_map<int, bool>& visit){
+        if(perm.size()==nums.size()){
+            res.push_back(perm);
+            return;
+        }
+
+        for(int i=idx; i<nums.size(); i++){
+            if(visit[i])   
+                continue;
+            visit[i]=true;
+            perm.push_back(nums[i]);
+            dfs(nums, idx, res, perm, visit);
+            perm.pop_back();
+            visit[i]=false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> perm;
+        unordered_map<int, bool> visit;
+        for(int i : nums)
+            visit[i]=false;
+        dfs(nums, 0, res, perm, visit);
+        return res;
+    }
+};
